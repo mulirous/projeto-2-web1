fefd403b4a8d8051cb0db0893ad294e1053a1063
mulirous <muriloalimacosta@hotmail.com>
2025-01-13 00:23:46 -0300

    Feat: Cabeçalho da página de contato

--- app/src/pages/faleCom/FaleCom.tsx
+++ app/src/pages/faleCom/FaleCom.tsx
@@ -7,14 +7,14 @@ export default function FaleCom() {
     return (
       <Layout>
         <div className="contactNumbers">
-          <div className="contactNumberText">
-            <img src="https://srzd.com/dashboard/wp-content/uploads/2024/04/calm.png" alt="ALGUMA IMAGEM" className="contactNumberTextImg" />
+          <div className="contactNumbersText">
+            <img src="https://srzd.com/dashboard/wp-content/uploads/2024/04/calm.png" alt="ALGUMA IMAGEM" className="contactNumbersTextImg" />
             <div className="contactNumebrTextInfos">
               <h1>Suporte</h1>
               <p>Algum questionamento ou precisa de assistencia? Venha até nós!</p>
             </div>
           </div>
-          <div className="contactNumberButtons">
+          <div className="contactNumbersButtons">
           <Botao to="/fale-com" className="callButton">Ligar</Botao>
           <Botao to="/fale-com" className="textButton">Enviar Mensagem</Botao>
           </div>
