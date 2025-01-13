69cddb485669c00dc0d9c42df3858a9a255e16fd
mulirous <muriloalimacosta@hotmail.com>
2025-01-13 00:16:02 -0300

    Feat: Estruturação cabeçalho da página de contato

--- app/src/pages/faleCom/FaleCom.tsx
+++ app/src/pages/faleCom/FaleCom.tsx
@@ -1,9 +1,32 @@
 import Layout from "../../layout/Layout";
+import Botao from "../../components/botao/Botao";
+
+import "./FaleCom.css"
 
 export default function FaleCom() {
     return (
       <Layout>
-        <h1> Fale Conosco</h1>
+        <div className="contactNumbers">
+          <div className="contactNumberText">
+            <img src="https://srzd.com/dashboard/wp-content/uploads/2024/04/calm.png" alt="ALGUMA IMAGEM" className="contactNumberTextImg" />
+            <div className="contactNumebrTextInfos">
+              <h1>Suporte</h1>
+              <p>Algum questionamento ou precisa de assistencia? Venha até nós!</p>
+            </div>
+          </div>
+          <div className="contactNumberButtons">
+          <Botao to="/fale-com" className="callButton">Ligar</Botao>
+          <Botao to="/fale-com" className="textButton">Enviar Mensagem</Botao>
+          </div>
+        </div>
+        <div className="contactAsks">
+          <div className="contacAsksText">
+
+          </div>
+          <div className="contactAsksForm">
+
+          </div>
+        </div>
       </Layout>
     );
   }
