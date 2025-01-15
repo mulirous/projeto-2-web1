af32563165871562e161cdf99532e63bc33cd1f2
pedro vitor <162462745+pdovitor@users.noreply.github.com>
2025-01-15 22:06:28 +0000

    sobre2

--- app/src/pages/sobre/Sobre.tsx
+++ app/src/pages/sobre/Sobre.tsx
@@ -12,21 +12,21 @@ export default function Sobre() {
       role: "CEO",
       image: PedroImage,
       description:
-        "Fundador 1 é o líder visionário da nossa equipe, trazendo décadas de experiência em gestão e inovação.",
+        "Pedro é o líder visionário da nossa equipe, trazendo consigo décadas de experiência em gestão e inovação. Sua visão estratégica e habilidades de liderança são fundamentais para guiar a equipe e a empresa rumo a novos horizontes, sempre com foco em soluções criativas e eficientes. Sua experiência nos permite enfrentar desafios com confiança e explorar oportunidades com visão de futuro.",
     },
     {
       name: "Lucas Melo",
       role: "CTO",
       image: LucasImage,
       description:
-        "Fundador 2 é o responsável por toda a tecnologia revolucionária que impulsiona nossa plataforma.",
+        "Lucas é o responsável pela tecnologia que impulsiona nossa plataforma, liderando o desenvolvimento e a implementação de soluções tecnológicas inovadoras. Seu trabalho incansável em aprimorar os sistemas que sustentam a nossa operação é essencial para manter a empresa na vanguarda do mercado. Com uma mente focada na transformação digital, Lucas garante que a tecnologia seja sempre um diferencial competitivo.",
     },
     {
       name: "Fundador 3",
       role: "Lead Developer",
       image: "https://srzd.com/dashboard/wp-content/uploads/2024/04/calm.png",
       description:
-        "Fundador 3 lidera a equipe de desenvolvimento, garantindo a excelência e a inovação em cada detalhe técnico.",
+        "Murilo lidera a equipe de desenvolvimento, sendo o pilar que garante a excelência em cada linha de código. Sua atenção aos detalhes técnicos e seu compromisso com a inovação asseguram que todos os projetos sejam entregues com a mais alta qualidade. Murilo se dedica a aprimorar constantemente os processos, trazendo melhorias contínuas e mantendo o padrão de excelência que caracteriza nossa equipe.",
     },
   ];
   return (
